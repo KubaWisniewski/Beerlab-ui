fff5329a9e26c82ba0fe8df1718db3249c49681f
JedrzejBablo <32323757+JedrzejBablo@users.noreply.github.com>
2019-11-17 04:21:59 +0100

    User interface

---
+++ src/Navbar.vue
@@ -0,0 +1,132 @@
+<template>
+    <div>
+        <v-navigation-drawer class = "background-color = grey darken-3" v-model="drawer" app clipped icon v-if="loggedIn">
+            <v-list dense>
+                <router-link to="/profile">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-action>
+                            <v-icon>mdi mdi-account-box</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Profile</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+                <router-link to="/menu">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-action>
+                            <v-icon>mdi-beer</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Beers</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+                <router-link to="/games">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-action>
+                            <v-icon>mdi-gamepad-variant</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Games</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+                <router-link to="/ranking">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-action>
+                            <v-icon>mdi-chart-line</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Ranking</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+                <router-link to="/currency">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-action>
+                            <v-icon>mdi-cash-multiple</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Currency</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+                <router-link to="/admin">
+                    <v-list-item @click="alert(123)">
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <span class="white--text">Add beers</span>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </router-link>
+            </v-list>
+        </v-navigation-drawer>
+        <notifications group="auth" position="bottom right" />
+        <v-app-bar app clipped-left class = "background-color = black darken-3">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="loggedIn"></v-app-bar-nav-icon>
+            <router-link to="/">
+                <v-icon large color="white">mdi-home</v-icon>
+            </router-link>
+            <v-spacer></v-spacer>
+            <template>
+                <router-link to="/cart">
+                    <v-btn icon v-if="loggedIn">
+                        <v-icon>mdi-cart-outline</v-icon>
+                    </v-btn>
+                </router-link>
+                <v-btn icon v-if="loggedIn">
+                    <v-icon @click="logout">mdi-logout</v-icon>
+                </v-btn>
+                <v-btn icon v-else>
+                    <router-link to="/login">
+                        <v-icon large color="white">mdi-account-circle</v-icon>
+                    </router-link>
+                </v-btn>
+            </template>
+        </v-app-bar>
+    </div>
+</template>
+
+
+<script>
+import { mapGetters } from "vuex";
+import { router } from "./router";
+
+export default {
+  components: {
+    name: "Footer"
+  },
+
+data() {
+    return {
+      drawer: false
+    };
+  },
+  computed: {
+    ...mapGetters({ loggedIn: "authentication/loggedIn" })
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("authentication/logout");
+    },
+    login() {
+      router.push("/login");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/user-pages/UserProfilePage.vue
+++ src/user-pages/UserProfilePage.vue
@@ -1,26 +1,57 @@
 <template>
-  <v-container class="fill-height text-center justify-center">
-    <div align-center>
-      <p style="font-size: 20px"></p>
-      <div v-if="this.user != null">
-        <v-avatar slot="offset" class="mx-auto d-block" size="130">
-          <img src="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg" />
-        </v-avatar>
-        <v-card-text class="text-xs-center">
-          <h4 class="card-title font-weight-light">{{user.username}}</h4>
-          <p
-            class="card-description font-weight-light"
-          >Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...</p>
-        </v-card-text>
-      </div>
-    </div>
-  </v-container>
+  <v-card
+    class="mx-auto background-color = grey lighten-1"
+    max-width="1000"
+    elevation = 3>
+    <v-container class="fill-height text-center justify-start">
+      <v-row justify="space-between">
+        <v-col cols="auto">
+          <div align-center>
+            <div v-if="this.user != null">
+              <v-avatar slot="offset" class="mx-auto d-block" size="250">
+                <img src="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg" />
+              </v-avatar>
+              <v-card-text class="text-xs-center">
+                <p class=" black--text display-2 font-weight-bold color=black">{{ user["username"] }}</p>
+              </v-card-text>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-data-table
+      :headers="headers"
+      :items="beers"
+      :items-per-page="5"
+      class="elevation-1">
+      <template v-slot:top>
+        <v-toolbar flat color="black">
+          <v-toolbar-title>Historia zamówień</v-toolbar-title>
+        </v-toolbar>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
+
+
 <script>
+import { mapGetters } from "vuex";
+
 const axios = require("axios");
 export default {
   data() {
     return {
+      headers: [
+          {
+            text: 'ID zamówienia',
+            align: 'left',
+            sortable: false,
+            value: 'id_zamowienia',
+          },
+          { text: 'Data'},
+          { text: 'Status'},
+          { text: 'Cena końcowa'},
+        ],
       user: {}
     };
   },
@@ -38,6 +69,12 @@ export default {
   },
   created() {
     this.fetchUserInfo;
+  },
+  computed: {
+    ...mapGetters({
+      user: "authentication/user",
+      loggedIn: "authentication/loggedIn"
+    })
   }
 };
 </script>
