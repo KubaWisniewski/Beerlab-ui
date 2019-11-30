14227949fef763d20654b9e824fad78f3743dc36
Daniel Belter <danielbelter96@gmail.com>
2019-11-30 17:27:30 +0100

    Empty cart should not be visible.

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,50 +1,43 @@
 <template>
   <v-container fluid class="pa-0">
     <v-row justify="center" align="center">
-      <v-img width="100%" max-height="300px" src="@/assets/koszyk.jpeg">
-      </v-img>
+      <v-img width="100%" max-height="300px" src="@/assets/koszyk.jpeg"></v-img>
     </v-row>
-    <v-row class=" text-center justify-center">
+    <v-row class="text-center justify-center">
       <div align-center>
-        <v-row :key="index" v-for="(orderItem, index) in order.orderItemsDto">
+        <div v-if="order.orderItemsDto.length == 0">Twój koszyk jest pusty.</div>
+        <div v-else>
+          <v-row :key="index" v-for="(orderItem, index) in order.orderItemsDto">
+            <v-card>
+              <v-card-title>{{ orderItem.beerDto.brand }}</v-card-title>
+              <v-card-text>
+                Opis: {{ orderItem.beerDto.description }}
+                <v-img height="200px" width="600" :src="orderItem.beerDto.imgUrl" />
+              </v-card-text>
+              <v-card-text
+                class="headline text-left text-left font-weight-bold large"
+              >Sztuk: {{ orderItem.quantity }}</v-card-text>
+              <v-card-text class="headline text-right font-weight-bold large">
+                Cena: {{ orderItem.beerDto.price }}
+                <v-icon large>mdi-beer</v-icon>
+              </v-card-text>
+            </v-card>
+          </v-row>
           <v-card>
-            <v-card-title>{{ orderItem.beerDto.brand }}</v-card-title>
-            <v-card-text>
-              Opis: {{ orderItem.beerDto.description }}
-              <v-img
-                height="200px"
-                width="600"
-                :src="orderItem.beerDto.imgUrl"
-              />
-            </v-card-text>
-            <v-card-text
-              class="headline text-left text-left font-weight-bold large"
-            >
-              Sztuk: {{ orderItem.quantity }}
-            </v-card-text>
-            <v-card-text class="headline text-right font-weight-bold large"
-              >Cena: {{ orderItem.beerDto.price }}
-              <v-icon large>mdi-beer</v-icon>
-            </v-card-text>
-          </v-card>
-        </v-row>
-        <v-card>
-          <v-card-title class="align-center"> Podsumowanie </v-card-title>
-          <v-card-text class="align-content-start">
-            Suma: {{ order.totalPrice }}
-          </v-card-text>
-          <v-card-actions class="justify-end">
-            <v-btn
-              @click="
+            <v-card-title class="align-center">Podsumowanie</v-card-title>
+            <v-card-text class="align-content-start">Suma: {{ order.totalPrice }}</v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn
+                @click="
                 setOrderStatus({ orderId: order.id, status: 'INPROGRESS' })
               "
-              rounded
-              color="green"
-              dark
-              >Potwierdz zamowienie</v-btn
-            >
-          </v-card-actions>
-        </v-card>
+                rounded
+                color="green"
+                dark
+              >Potwierdz zamowienie</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
       </div>
     </v-row>
   </v-container>
